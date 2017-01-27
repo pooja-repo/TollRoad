f574150b7b76a1d2d85e90a756e2d0164a45309e
Pooja Verma <poojaverma9229@gmail.com>
2017-01-27 11:13:27 +0530

    commit 1

---
+++ src/com/poja/example/Test2.java
@@ -0,0 +1,29 @@
+package com.poja.example;
+
+public class Test2 {
+
+	public static void isPalindrom(String input) {
+		String str = input;
+		String temp = "";
+		
+		//char ch[] = str.toCharArray();//pooja aoojp
+		int length = str.length()- 1;
+		
+		for(int i = length; i >= 0; i-- ){
+			temp = temp + str.charAt(i);
+		}
+		
+		
+		if(temp == input){
+			System.out.println(temp + " This is a palindrom");
+		}
+			
+		else
+			System.out.println(temp + " Is Not palindrom");
+	}
+
+	public static void main(String[] args) {
+		isPalindrom("dad");
+
+	}
+}
